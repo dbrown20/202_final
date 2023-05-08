47431e66b05e34cfc315a162e6dfcffae77586b7
tyler <tylerscott.home@gmail.com>
2023-05-08 12:30:27 -0400

    Reveal Casts

--- base/compiler.py
+++ base/compiler.py
@@ -92,6 +92,8 @@ def cast_insert(program: Program) -> Program:
             case Constant(n):
                 if isinstance(n, int):
                     return Prim('inject', [Constant(n), int])
+                if isinstance(n, str):
+                    return Prim('inject', [Constant(n), str])
                 elif isinstance(n, bool):
                     return Prim('inject', [Constant(n), bool])
                 else:
@@ -115,6 +117,8 @@ def cast_insert(program: Program) -> Program:
             case Constant(n):
                 if isinstance(n, int):
                     return Prim('inject', [Constant(n), int])
+                elif isinstance(n, str):
+                    return Prim('inject', [Constant(n), str])
                 elif isinstance(n, bool):
                     return Prim('inject', [Constant(n), bool])
                 else:
@@ -318,48 +322,6 @@ def reveal_casts(program: Program) -> Program:
     # the value is converted to a value of the target type by removing the tag; if it does
     # not, the program exits
 
-    # x = project(, int)
-
-    # if any_t.tag == int:
-    #     return any_t.val
-    # if any_t.tag == bool:
-    #     return any_t.val
-    # else:
-    #     exit()
-
-    # The reveal_casts pass compiles project and inject.
-    # 1. Project compiles into an if statement that checks if the tag is correct and returns the value
-    # 2. Inject compiles into the 'make_any' primitive that attaches a tag (select-instructions will compile it further)
-
-    # def rc_expr(e: Expr) -> Expr:
-    #     match e:
-    #         case Var(x):
-    #             return Var(x)
-    #         case Constant(n):
-    #             if isinstance(n, int):
-    #                 return Prim('inject', [Constant(n), int])
-    #             elif isinstance(n, bool):
-    #                 return Prim('inject', [Constant(n), bool])
-    #             else:
-    #                 raise Exception('error!')
-    #         case Prim(op, args):
-    #             new_args = [rc_expr(e) for e in args]
-    #             return Prim(op, new_args)
-    #         case _:
-    #             raise Exception('ci_expr', e)
-    #
-    # def rc_prim(prims: Prim) -> AnyVal:
-    #     match prims:
-    #         case Prim('project', [e, t]):
-    #             if e.tag == t:
-    #                 prims = e.val
-    #                 return prims
-    #             else:
-    #                 exit()
-    #         case _:
-    #             raise Exception('rc_prim', prims)
-
-
     def mk_tag(types: Tuple[type]) -> int:
         """
         Builds a vector tag. See section 5.2.2 in the textbook.
@@ -414,6 +376,12 @@ def reveal_casts(program: Program) -> Program:
                 return [Assign(x, e.val)]
             case Assign(x, Prim('inject', [e, t])):
                 return [Assign(x, Prim('make_any', [e, t]))]
+            # Inject(e, ftype)
+            # ⇒
+            # Call(Name('make_any'), [e′, Constant(tagof (ftype))])
+            case Assign (x, Prim('inject', [e, t])):
+
+                return [Assign(x, Prim('make_any', [e, Constant(mk_tag(t))]))]
             # TODO: END
 
             case If(cond, then_stmts, else_stmts):
